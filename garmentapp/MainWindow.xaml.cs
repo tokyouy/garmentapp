8274123921f56b008bc42e975c60358b02ef8018
tokyouy <74891946+tokyouy@users.noreply.github.com>
2022-05-07 12:17:58 +0900

    Добавьте файлы проекта.

---
+++ garmentapp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data.Entity;
+
+namespace garmentapp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        user validate = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void ValidateUser(string login, string password)
+        {
+
+            validate = garmentEntities.GetContext().users.Where(p => p.login == login && p.password == password).FirstOrDefault();
+            if (validate != null)
+            {
+                userWin Userwin = new userWin();
+                Userwin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль не верен!");
+            }
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = tbLogin.Text;
+            string Password = pbPass.Password;
+            ValidateUser(Login, Password);
+        }
+
+        private void RegisterBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            regWin regWindow = new regWin();
+            regWindow.Show();
+        }
+
+        
+    }
+}
---
+++ garmentapp/regWin.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace garmentapp
+{
+    /// <summary>
+    /// Логика взаимодействия для regWin.xaml
+    /// </summary>
+    public partial class regWin : Window
+    {
+        private user _currentUser = new user();
+        public regWin()
+        {
+            InitializeComponent();
+            DataContext = _currentUser;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (String.IsNullOrEmpty(tbfName.Text))
+                errors.AppendLine("Введите имя");
+            if (String.IsNullOrEmpty(tbsName.Text))
+                errors.AppendLine("Введите фамилию");
+            if (String.IsNullOrEmpty(tbLogin.Text))
+                errors.AppendLine("Введите логин");
+            if (String.IsNullOrEmpty(tbPassword.Text))
+                errors.AppendLine("Введите пароль");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (garmentEntities.GetContext().users.Where(p => p.login == tbLogin.Text).Count() == 0)
+            {
+                if (_currentUser.id == 0)
+                    _currentUser.roleid = 2;
+                garmentEntities.GetContext().users.Add(_currentUser);
+
+                try
+                {
+                    garmentEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует!");
+            }
+
+        }
+    }
+}
